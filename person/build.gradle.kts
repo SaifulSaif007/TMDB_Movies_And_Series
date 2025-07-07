1dea8c604c1faaacf5c0e6a62f87f4ce0ad3e957
SaifulSaif007 <saifulsaif321@gmail.com>
2025-07-08 01:15:43 +0600

    gradle files updated to Kotlin DSL

---
+++ person/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+apply(from = "${project.rootDir}/jacoco.gradle")
+
+android {
+    namespace = "com.saiful.person"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            isTestCoverageEnabled = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":base"))
+    implementation(project(":shared"))
+    testImplementation(project(":base-unit-test"))
+
+    implementation(libs.dagger.hilt)
+    implementation(libs.bundles.androidxNavigation)
+    kapt(libs.hilt.compiler)
+} 
